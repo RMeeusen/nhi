b378f7fb1b8e77cdf4c8994fe728cd6d3fedf1fa
wschoonveld <35798126+wschoonveld@users.noreply.github.com>
2020-07-02 16:47:34 +0200

    1. de hoofdjob verwerkt nu data voor alle waterschappen waarvoor in de tabel administratiefgebied.active=true 2. update_administratiefgebied.sql: wijzigingen tov prod

---
+++ gml2db/scripts/update_administratiefgebied.sql
@@ -0,0 +1,34 @@
+alter table public.administratiefgebied add if not exists importdir varchar;
+
+delete from public.administratiefgebied where administratiefgebiedid in (15,23);
+
+update public.administratiefgebied set administratiefgebied = 'limburg' where administratiefgebiedid =11;
+
+update public.administratiefgebied set importdir = case 
+ when administratiefgebiedid = 1 then 'aaenmaas'
+ when administratiefgebiedid = 2 then 'amstelgooienvecht'
+ when administratiefgebiedid = 3 then 'brabantsedelta'
+ when administratiefgebiedid = 4 then 'delfland'
+ when administratiefgebiedid = 5 then 'dedommel'
+ when administratiefgebiedid = 6 then 'wetterskipfryslan'
+ when administratiefgebiedid = 7 then 'hollandsnoorderkwartier'
+ when administratiefgebiedid = 8 then 'hollandsedelta'
+ when administratiefgebiedid = 9 then 'hunzeenaas'
+ when administratiefgebiedid = 10 then 'noorderzijlvest'
+ when administratiefgebiedid = 11 then 'limburg'
+ when administratiefgebiedid = 12 then 'rijnenijssel'
+ when administratiefgebiedid = 13 then 'rijnland'
+ when administratiefgebiedid = 14 then 'rivierenland'
+ when administratiefgebiedid = 16 then 'schielandendekrimepenerwaard'
+ when administratiefgebiedid = 17 then 'stichtserijnlanden'
+ when administratiefgebiedid = 18 then 'zuiderzeeland'
+ when administratiefgebiedid = 19 then 'scheldestromen'
+ when administratiefgebiedid = 20 then 'valleienveluwe'
+ when administratiefgebiedid = 21 then 'vechtstromen'
+ when administratiefgebiedid = 22 then 'drentsoverijsselsedelta'
+ end;
+
+alter table administratiefgebied add if not exists active bool;
+
+update administratiefgebied  set active = true where administratiefgebiedid in (1,5,11,14,17,20);
+
